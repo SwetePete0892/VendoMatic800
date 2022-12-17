99225a5211d561d58784068885b5ea79055fbf0b
Peter Johnson <swetepete92@gmail.com>
2022-12-17 10:09:18 -0500

    Updated Log Class

--- src/main/java/com/techelevator/view/Log.java
+++ src/main/java/com/techelevator/view/Log.java
@@ -1,5 +1,6 @@
 package com.techelevator.view;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
@@ -10,23 +11,37 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Map;
 
+import static java.time.format.DateTimeFormatter.BASIC_ISO_DATE;
+
 public class Log {
 
+    // Properties
+    private static final File THEFT_REPORT_DIRECTORY = new File("theftLogs/");
+    private static final File SALES_REPORT_DIRECTORY = new File("salesReportLogs/");
     private static PrintWriter theftWriter;
+    private static PrintWriter salesReportWriter;
     private static final String TIMESTAMP = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT).format(LocalDateTime.now());
+    private static final String DATESTAMP = LocalDate.now().format(BASIC_ISO_DATE);
+
+    /* Logs instances of transactions: feedMoney, purchaseItem, makeChange. Creates a Printwriter on initial use.
+     * Creates a directory to hold the reports if one does not exist. New files are created daily.
+     * If a file exists for the day, appends to that file. All transactions are timestamped. */
 
     public static void theftLog(String action, BigDecimal transactionAmount, BigDecimal totalMoney) {
-        // Trying to reduce how many times we need to open and close a PrintWriter
-            // With the naming convention, I hope to build a folder that holds all the theft logs.
-            // It creates a new log for every day. If a log for the current day exists, it simply appends to it.
 
         try{
+
+            if(THEFT_REPORT_DIRECTORY.mkdir()){
+                // Creates directory for theft reports if it doesn't exist.
+            }
+
             if(theftWriter == null){
-                theftWriter = new PrintWriter(new FileOutputStream(LocalDate.now() + "theftLog.log",true), true);
+                theftWriter = new PrintWriter(new FileOutputStream(
+                        THEFT_REPORT_DIRECTORY + "/" + DATESTAMP + "theftLog.log",true), true);
             }
 
             //I need to go back and learn about local currency and how to use that
-            theftWriter.println(TIMESTAMP + " " + action + " $" + transactionAmount + " $" + totalMoney);
+            theftWriter.println(TIMESTAMP + " | " + action + " | $" + transactionAmount + " | $" + totalMoney);
 
         } catch (FileNotFoundException nfe){
             System.out.println(nfe.getMessage());
@@ -42,10 +57,22 @@ public class Log {
     // I think it works for the scope of the project because we never exceed 5 or restock
     // I plan to think of a better method that can handle restocking.
     public static void salesReport(Map<String, VendingItem> remainingInventory){
-        try(PrintWriter salesWriter = new PrintWriter(new FileOutputStream(LocalDate.now() + "salesReport.txt", true))) {
+
+        try {
+
+            if(SALES_REPORT_DIRECTORY.mkdir()){
+                // Creates sales report directory if it doesn't exist.
+            }
+
+            if(salesReportWriter == null){
+                salesReportWriter = new PrintWriter(new FileOutputStream(
+                        SALES_REPORT_DIRECTORY + "/" + DATESTAMP + "salesReport.log",true), true);
+            }
+
             BigDecimal totalSales = new BigDecimal("0.00");
 
-            salesWriter.println("SALES REPORT: " + TIMESTAMP);
+            salesReportWriter.println("SALES REPORT: " + TIMESTAMP);
+            salesReportWriter.println("---------------------------");
 
             for (Map.Entry<String, VendingItem> individualUnit : remainingInventory.entrySet()) {
                 // For the sanity of the reader, values are put into variables
@@ -57,16 +84,16 @@ public class Log {
                     int unitsSold = 5 - currentStock;
                     BigDecimal unitsSoldDecimal = new BigDecimal(unitsSold);
                     totalSales = totalSales.add(itemPrice.multiply(unitsSoldDecimal));
-                    System.out.println(itemName + " | " + unitsSold);
-                    salesWriter.println(itemName + " | " + unitsSold);
+                    salesReportWriter.printf("%-20s | %2d\n", itemName, unitsSold);
+                    salesReportWriter.println("---------------------------");
 
                 } else {
-                    System.out.println(itemName + " | " + 0);
-                    salesWriter.println(itemName + " | " + 0);
+                    salesReportWriter.printf("%-20s | %2d\n", itemName, 0);
+                    salesReportWriter.println("---------------------------");
                 }
             }
-            System.out.println("**TOTAL SALES** " + totalSales);
-            salesWriter.println("**TOTAL SALES** " + totalSales + "\n");
+            salesReportWriter.println(" **TOTAL SALES** $" + totalSales + "\n");
+            System.out.println("\n**Generated sales report**");
 
         } catch (Exception e) {
             System.out.println("Something went wrong with the Sales Report");
